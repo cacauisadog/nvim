00a2314259b5572e93f494b96740a70f2b72a2bc
cacauisadog <cbcaue@protonmail.com>
2024-05-28 15:40:16 -0300

    add "venv-selector" plugin and dependencies

---
+++ lua/caue/plugins/venv-selector.lua
@@ -0,0 +1,21 @@
+return {
+	"linux-cultist/venv-selector.nvim",
+	dependencies = {
+		"neovim/nvim-lspconfig",
+		"mfussenegger/nvim-dap",
+		"mfussenegger/nvim-dap-python", --optional
+		{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+	},
+	opts = {
+		-- Your options go here
+		-- name = "venv",
+		-- auto_refresh = false
+	},
+	event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+	keys = {
+		-- Keymap to open VenvSelector to pick a venv.
+		{ "<leader>vs", "<cmd>VenvSelect<cr>" },
+		-- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+		{ "<leader>vc", "<cmd>VenvSelectCached<cr>" },
+	},
+}
