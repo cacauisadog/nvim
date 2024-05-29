a9f1ce22f7933f5aff845346f98bd56522093f0a
cacauisadog <cbcaue@protonmail.com>
2024-05-29 18:25:48 -0300

    add more diagnostics keymaps and adjust some existing ones

--- lua/caue/plugins/lsp/lspconfig.lua
+++ lua/caue/plugins/lsp/lspconfig.lua
@@ -42,28 +42,35 @@ return {
 				keymap.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
 
 				opts.desc = "See available code actions"
-				keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+				keymap.set({ "n", "v" }, "<leader>la", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
 				opts.desc = "Smart rename"
-				keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts) -- smart rename
+				keymap.set("n", "<leader>lr", vim.lsp.buf.rename, opts) -- smart rename
 
 				opts.desc = "Show buffer diagnostics"
-				keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
-
-				opts.desc = "Show line diagnostics"
-				keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
-
-				opts.desc = "Go to previous diagnostic"
-				keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
-
-				opts.desc = "Go to next diagnostic"
-				keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+				keymap.set("n", "<leader>lD", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
 
 				opts.desc = "Show documentation for what is under cursor"
 				keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 
 				opts.desc = "Restart LSP"
-				keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+				keymap.set("n", "<leader>lL", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+
+				-- diagnostics
+				local diagnostic_goto = function(next, severity)
+					local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
+					severity = severity and vim.diagnostic.severity[severity] or nil
+					return function()
+						go({ severity = severity })
+					end
+				end
+				keymap.set("n", "<leader>ld", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
+				keymap.set("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
+				keymap.set("n", "[d", diagnostic_goto(false), { desc = "Prev Diagnostic" })
+				keymap.set("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
+				keymap.set("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
+				keymap.set("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
+				keymap.set("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
 			end,
 		})
 
