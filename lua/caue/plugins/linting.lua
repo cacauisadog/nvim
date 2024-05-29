86cd24d70c3ff837df5ae7950b0b42b8b88b7c95
cacauisadog <cbcaue@protonmail.com>
2024-05-29 16:54:55 -0300

    lint htmldjango with djlint

--- lua/caue/plugins/linting.lua
+++ lua/caue/plugins/linting.lua
@@ -11,6 +11,7 @@ return {
 			typescriptreact = { "eslint_d" },
 			vue = { "eslint_d" },
 			python = { "flake8" },
+			htmldjango = { "djlint" },
 		}
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
