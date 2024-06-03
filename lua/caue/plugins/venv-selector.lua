2fe93e86e99d9e54b9d42ca3c8ec4a933e745661
Cauê Bittencourt Carnietto <cbcaue@protonmail.com>
2024-06-03 08:47:59 -0300

    update `venv-selector` plugin to the "regexp" branch
    And adjust settings accordingly so I can find my custom pyenv
    virtualenvs and not have any conflicts with Mason.

--- lua/caue/plugins/venv-selector.lua
+++ lua/caue/plugins/venv-selector.lua
@@ -6,16 +6,35 @@ return {
 		"mfussenegger/nvim-dap-python", --optional
 		{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
 	},
-	opts = {
-		-- Your options go here
-		-- name = "venv",
-		-- auto_refresh = false
-	},
-	event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+	branch = "regexp", -- use this for new version
+	event = "VeryLazy", -- this is needed to avoid conflicts with Mason
+	config = function()
+		require("venv-selector").setup({
+			settings = {
+				options = {
+					debug = false, -- enables you to run the VenvSelectLog command to view debug logs
+					on_telescope_result_callback = nil, -- callback function for when a search result shows up in telescope
+					on_venv_activate_callback = nil, -- callback function for when a venv is activated
+					fd_binary_name = nil, -- plugin looks for `fd` or `fdfind` but you can set something else here
+					enable_default_searches = true, -- switches all default searches on/off
+					enable_cached_venvs = true, -- automatically activates the venv you used last time in a directory
+					activate_venv_in_terminal = true, -- activate the selected python interpreter in terminal windows opened from neovim
+					set_environment_variables = true, -- sets VIRTUAL_ENV or CONDA_PREFIX environment variables
+					show_telescope_search_type = true, -- shows the name of the search in telescope
+					notify_user_on_venv_activation = true, -- notifies user on activation of the virtual env
+					search_timeout = 5, -- if a search takes longer than this many seconds, stop it and alert the user
+				},
+				search = {
+					-- this is my custom `fd` command to search for the virtualenvs I've created with `pyenv virtualenv x.x.x venv-name`
+					pyenv_venvs = {
+						command = "fd 'versions/.*[0-9a-z_-]+.*/bin/python$' ~/.pyenv/versions --full-path --color never -E /proc",
+					},
+				},
+			},
+		})
+	end,
 	keys = {
 		-- Keymap to open VenvSelector to pick a venv.
 		{ "<leader>vs", "<cmd>VenvSelect<cr>" },
-		-- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
-		{ "<leader>vc", "<cmd>VenvSelectCached<cr>" },
 	},
 }
