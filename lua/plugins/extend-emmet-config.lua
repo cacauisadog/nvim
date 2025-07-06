7a67d7dc41e7e2f1eeb7cc4987927af93359d2d4
cacauisadog <cbcaue@protonmail.com>
2025-07-06 10:33:10 -0300

    configure emmet plugin

---
+++ lua/plugins/extend-emmet-config.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        emmet_ls = {
+          filetypes = {
+            "css",
+            "eruby",
+            "html",
+            "javascript",
+            "javascriptreact",
+            "typescriptreact",
+            "less",
+            "sass",
+            "scss",
+            "svelte",
+            "pug",
+            "vue",
+          },
+        },
+      },
+    },
+  },
+}
