55968f8241d04f84f7eea32da68d68a0056c7f52
cacauisadog <cbcaue@protonmail.com>
2025-09-01 08:30:10 -0300

    change venv-selector default branch to "main"

--- lua/plugins/venv-selector.lua
+++ lua/plugins/venv-selector.lua
@@ -1,12 +1,12 @@
 return {
   "linux-cultist/venv-selector.nvim",
-  branch = "regexp", -- Use this branch for the new version
+  branch = "main",
   cmd = "VenvSelect",
   enabled = true,
   opts = {
     settings = {
       options = {
-        notify_user_on_venv_activation = false,
+        notify_user_on_venv_activation = true,
       },
     },
   },
